7ec58db7b1a8ec8596d29e146474e31cd413f78c
jlambert12013 <jlambert12013@gmail.com>
2024-03-12 02:18:06 -0500

    updating at 53:00

--- src/arrays.rs
+++ src/arrays.rs
@@ -1,6 +1,24 @@
+use std::mem;
+
 // Arrays - Fixed list where elements are the same data types
 
 pub fn run() {
-    let number_array: [i32; 5] = [1, 2, 3, 4, 5];
-    println!("{:?}", number_array)
+    let mut number_array: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", number_array);
+
+    // Getting a single value
+    println!("Single Value: {}", number_array[0]);
+    println!("Single Value: {}", number_array[2]);
+    println!("Single Value: {}", number_array[4]);
+
+    // Reassign Value
+    number_array[4] = 23;
+    println!("{}", number_array[4]);
+
+    // Get array length
+    println!("Length of array: {}", number_array.len());
+
+    // Get amount of memory
+    // Arrays are stack allocated
+    println!("Array occupies {} bytes.", mem::size_of_val(&number_array));
 }
